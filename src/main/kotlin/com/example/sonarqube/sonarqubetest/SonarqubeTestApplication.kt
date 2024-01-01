f9bc4b781e45a775101ce0e709ce85148ba0d60d
joongsoopark <wndtn853@gmail.com>
2024-01-01 17:24:56 +0900

    test-commit

--- src/main/kotlin/com/example/sonarqube/sonarqubetest/SonarqubeTestApplication.kt
+++ src/main/kotlin/com/example/sonarqube/sonarqubetest/SonarqubeTestApplication.kt
@@ -10,5 +10,6 @@ fun main(args: Array<String>) {
     println("Hello, world!")
     println("Hello, world!")
     println("Hello, world!")
+    println("Hello, world!")
     runApplication<SonarqubeTestApplication>(*args)
 }
