43e2152107c084ac0aff2c1b07efcc18d9293b0e
joongsoopark <wndtn853@gmail.com>
2024-01-02 01:49:51 +0900

    J1

--- src/main/kotlin/com/example/sonarqube/sonarqubetest/RedisConfig.java
+++ src/main/kotlin/com/example/sonarqube/sonarqubetest/RedisConfig.java
@@ -24,6 +24,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 public class RedisConfig {
