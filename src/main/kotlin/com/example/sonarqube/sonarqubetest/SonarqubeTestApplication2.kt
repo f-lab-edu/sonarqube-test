b0508200932105c3c5558cd330754210e106f092
joongsoopark <wndtn853@gmail.com>
2024-01-01 17:32:34 +0900

    키 변경

---
+++ src/main/kotlin/com/example/sonarqube/sonarqubetest/SonarqubeTestApplication2.kt
@@ -0,0 +1,15 @@
+package com.example.sonarqube.sonarqubetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SonarqubeTestApplication
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+    println("Hello, world!")
+    println("Hello, world!")
+    println("Hello, world!")
+    runApplication<SonarqubeTestApplication>(*args)
+}
