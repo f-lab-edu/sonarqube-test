cb4fbe8454d5a6dde1d1476357b3d889c0dee571
joongsoopark <wndtn853@gmail.com>
2024-01-01 16:51:04 +0900

    initial-commit

---
+++ src/main/kotlin/com/example/sonarqube/sonarqubetest/SonarqubeTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sonarqube.sonarqubetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SonarqubeTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SonarqubeTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sonarqube/sonarqubetest/SonarqubeTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sonarqube.sonarqubetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SonarqubeTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
